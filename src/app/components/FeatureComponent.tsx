c3a651fa557d691f8a021c38b86447f998fbaa10
KashafYasir <kashafyasir21@gmail.com>
2024-12-09 21:56:10 +0500

    Task-0001: FigmaTemplate | Initial commit of project

---
+++ src/app/components/FeatureComponent.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../styles/FeatureComponent.css';
+
+const featuresData = [
+  {
+    id: 1,
+    title: 'High Quality',
+    description: 'crafted from top materials',
+    imageId: '117:613',
+    imageUrl:'https://w7.pngwing.com/pngs/156/101/png-transparent-award-cup-prize-trophy-winning-education-and-school-glyph-px-icon.png'
+  },
+  {
+    id: 2,
+    title: 'Warranty Protection',
+    description: 'Over 2 years',
+    imageId: '117:621',
+    imageUrl:'https://png.pngtree.com/png-vector/20210307/ourlarge/pngtree-illustration-of-warranty-badge-icon-good-for-packaging-png-image_3016735.png'
+  },
+  {
+    id: 3,
+    title: 'Free Shipping',
+    description: 'Order over 150 $',
+    imageId: '117:629',
+    imsgeUrl:'https://th.bing.com/th/id/OIP.vBWn-cLjNMhLJ7DJJJ3ewAAAAA?rs=1&pid=ImgDetMain'
+  },
+  {
+    id: 4,
+    title: '24 / 7 Support',
+    description: 'Dedicated support',
+    imageId: '117:635',
+    imageUrl:'https://thumbs.dreamstime.com/b/customer-support-helpdesk-technician-help-logo-symbol-icon-hotline-service-headphones-249464479.jpg'
+  },
+];
+
+const FeaturesComponent: React.FC = () => {
+  return (
+    <div className="features-container">
+      {featuresData.map(feature => (
+        <div key={feature.id} className="feature-item">
+          <img src={`https://placeholder.pics/svg?${feature.imageId}`} alt={feature.title} width="60" height="60" />
+          <div className="feature-text">
+            <h3>{feature.title}</h3>
+            <p>{feature.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FeaturesComponent;
+
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/HeroSection.css';
+
+const Hero: React.FC = () => {
+  return (
+    <div id='hero' className="hero-section">
+      
+        
+      <div className="hero-content">
+        <h4>New Arrival</h4>
+        <h1>Discover Our New Collection</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <div className="container">
+          <button className='button'>Buy Now</button>
+        </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Hero;
+
